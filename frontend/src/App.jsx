4063fd5ed2f63897d27cfab90f370206bc14926e
SRIT4M <itssritamjena@gmail.com>
2025-07-17 22:39:37 +0530

    Fix Tailwind/DaisyUI integration and file structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,57 @@
+import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+
+import { Routes, Route, Navigate } from "react-router-dom";
+import { axiosInstance } from "./lib/axios";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+
+import { Loader } from "lucide-react";
+
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
-    <div className="text-red-500">
-      Tailwind is working! 🚀
-      <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind</h1>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
     </div>
   );
 };
